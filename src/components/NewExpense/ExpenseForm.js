d7828b53cc19e59d943e6f4bd6bd7056ca679e29
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-03-26 20:12:19 +0300

    started challenge from last chapter saving to continue on laptop

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -32,17 +32,36 @@ const ExpenseForm = () => {
     //   return { ...prevState, enteredDate: e.target.value };
     // });
   };
+  const submithandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredAmount("");
+    setEnteredTitle("");
+    setEnteredDate("");
+  };
   return (
-    <form>
+    <form onSubmit={submithandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             onChange={amountChangeHandler}
             min="0.01"
             step="0.01"
@@ -52,6 +71,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             onChange={dateChangeHandler}
             min="2019-01-01"
             max="2024-12-31"
