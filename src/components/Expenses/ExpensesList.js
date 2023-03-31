d4c23405528ceb616afb53dc4214e599318a0b4b
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-03-31 15:24:41 +0300

    i finished my expenses app i added a new feature witch makes add new expense as a dropdown menu and i addded the pills wich fill up for month that we spent the most

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,7 +2,7 @@ import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 const ExpensesList = (props) => {
-  if (props.items === 0) {
+  if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
   return (
